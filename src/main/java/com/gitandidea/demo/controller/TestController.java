ae2502d70f9333d46009e649143c3e20ed55fc4b
hxloo <1763660078@qq.com>
2020-08-19 09:50:57 +0800

    项目经理第一次提交项目GitAndIDEA

---
+++ src/main/java/com/gitandidea/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gitandidea.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String test01(){
+        return "hello world";
+    }
+}
